fdb58271f1f6e9202612500091d9d529207d49ec
Esemega <sogaral89@gmail.com>
2021-06-28 21:26:51 +0200

    first commit

---
+++ src/common/components/seo.component.tsx
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
+
+interface Props {
+  title: string;
+  description?: string;
+  lang?: string;
+  meta?: any[];
+  keywords?: string[];
+}
+
+const query = graphql`
+  query DefaultSEOQuery {
+    site {
+      siteMetadata {
+        title
+        description
+        author
+      }
+    }
+  }
+`;
+
+export const SEO: React.FC<Props> = (props) => {
+  const { description, lang, meta, keywords, title } = props;
+  const data = useStaticQuery(query);
+  const metaDescription = description || data.site.siteMetadata.description;
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={`%s | ${data.site.siteMetadata.title}`}
+      meta={[
+        {
+          name: 'description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:title',
+          content: title,
+        },
+        {
+          property: 'og:description',
+          content: metaDescription,
+        },
+        {
+          property: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary',
+        },
+        {
+          name: 'twitter:creator',
+          content: data.site.siteMetadata.author,
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          content: metaDescription,
+        },
+      ]
+        .concat(
+          keywords.length > 0
+            ? {
+                name: 'keywords',
+                content: keywords.join(', '),
+              }
+            : []
+        )
+        .concat(meta)}
+    />
+  );
+};
+
+SEO.defaultProps = {
+  lang: 'en',
+  meta: [],
+  keywords: [],
+};
---
+++ src/pods/home/home.component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+import Typography from '@material-ui/core/Typography';
+import { routes } from 'core/routes';
+import * as classes from './home.styles';
+
+const query = graphql`
+  query {
+    homeLogo: file(relativePath: {eq: "home-logo.png"}) {
+      childImageSharp {
+        gatsbyImageData(layout:CONSTRAINED, placeholder: BLURRED)
+      }
+    }
+  }
+`;
+
+export const Home: React.FunctionComponent = () => {
+  const { homeLogo} = useStaticQuery(query);
+  return (
+    <div className={classes.root}>
+      <Typography variant="h1">Welcome to this website</Typography>
+      <GatsbyImage
+        image={getImage(homeLogo)}
+        alt="Lemocode logo"
+      />
+
+      <Typography variant="h2">
+        Check out our <Link to={routes.blog}>blog</Link>
+      </Typography>
+    </div>
+  );
+};
